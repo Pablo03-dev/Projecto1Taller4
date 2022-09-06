9ed53f33b09811fb69505221726d95eeedbbbad3
Pablo03-dev <84738819+Pablo03-dev@users.noreply.github.com>
2022-09-05 23:08:59 -0300

    datos ui

---
+++ Assets/Scripts/HPSystem.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HPSystem : MonoBehaviour
+{
+	[SerializeField] private int health = 3;
+	[SerializeField] private int maxHealth = 3;
+
+	public Lives vida_canvas;
+	public int vida = 3;
+
+	private void Start()
+	{
+		maxHealth = health;
+
+		vida_canvas = GameObject.FindObjectOfType<Lives>(); 
+	}
+
+	
+
+	public void PlusHealth(int amount)
+	{
+		if (health + amount > maxHealth)
+		{
+			health = maxHealth;
+		}
+
+		vida_canvas.CambioVida((int)health);
+	}
+
+	public void TakeHealth(int amount)
+    {
+		health -= amount;
+
+		if (health <= 0)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	public void ModifyHealth(int amount)
+	{
+		if (health + amount > maxHealth)
+		{
+			amount = maxHealth - health;
+		}
+
+		health += amount;
+
+		//DEAD
+		if (health <= 0)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+    private void Update()
+    {
+        //vida_canvas.CambioVida(vida);
+    }
+}
