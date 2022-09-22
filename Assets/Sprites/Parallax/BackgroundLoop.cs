ba50ddab08b9f41b52454c1d9444382efa609a75
Pablo03-dev <84738819+Pablo03-dev@users.noreply.github.com>
2022-09-21 22:12:48 -0300

    aaaa

---
+++ Assets/Sprites/Parallax/BackgroundLoop.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundLoop : MonoBehaviour
+{
+    [SerializeField] private Material mat;
+    [SerializeField] private float parallax = 2f;
+    private Vector2 offset;
+    private SpriteRenderer sp;
+
+
+    void Start()
+    {
+        sp = GetComponent<SpriteRenderer>();
+        mat = sp.material;
+    }
+
+    void Update()
+    {
+        offset.x += Time.deltaTime / parallax;
+        mat.mainTextureOffset = offset;
+    }
+}
